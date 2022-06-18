7e0677030420931532b8fea31d99169e1c2b4923
Anu <annuv231@gmail.com>
2022-06-18 20:12:03 +0530

    added ignore file and DOTEnv

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let weather = {
-  "api-key": API_KEY,
+  "api-key": "9816f84cfdd59a214b445a1312899350",
   fetchWeather: function (city) {
     fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this["api-key"]}`
@@ -12,7 +12,7 @@ let weather = {
     const { description, icon } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
-    console.log(name, description, icon, temp, humidity, speed);
+    //console.log(name, description, icon, temp, humidity, speed);
     document.querySelector(".city").innerText = name;
     document.querySelector(".temprature").innerText = temp + "°C";
     document.querySelector(
@@ -33,6 +33,8 @@ const btn = document.querySelector(".search button");
 btn.addEventListener("click", function () {
   weather.search();
 });
+
+// trigging action with ENTER key
 document.querySelector("#searchBox").addEventListener("keypress", (event) => {
   if (event.keyCode === 13) {
     // key code of the keybord key
@@ -41,3 +43,8 @@ document.querySelector("#searchBox").addEventListener("keypress", (event) => {
     // your code to Run
   }
 });
+window.onload = function () {
+  weather.fetchWeather("New Delhi");
+  // fetchWeather("New delhi");s
+  console.log("hellos");
+};
